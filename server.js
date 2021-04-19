4b020b5364cf905ab16675a1216abc65a53711da
jana <vs.janarthani@gmail.com>
2021-04-18 23:31:58 -0700

    added codes to accept prompt and display results

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const mysql = require('mysql2');
 const queries = require('./lib/queries');
-// const {getDepts, getRoles, getEmps} = require('./lib/function');
+
 
 // Connect to database
-const db = mysql.createConnection(
+const dbpool = mysql.createPool(
     {
         host: 'localhost',
         user: 'janarthani',
         password: 'password',
-        database: 'employee_db'
+        database: 'employee_db',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
     });
 
+initialPrompt();
+
 // Initial prompt
-const initialPrompt = () => {
-    return inquirer
+function initialPrompt() {
+    inquirer
         .prompt([
             {
                 type: 'list',
@@ -29,20 +34,37 @@ const initialPrompt = () => {
                     'Add a role',
                     'Add an employee',
                     'Update an employee role',
-                    'quit',
+                    'Quit',
                 ]
+
             },
             {
                 type: 'input',
                 name: 'dep_name',
-                message: 'Enter the name of the Department:',
-                when: ({ choice }) => choice === 'Add a department'
+                message: 'Enter the name of the Department: (Required)',
+                when: ({ choice }) => choice === 'Add a department',
+                validate: dep_nameInput => {
+                    if (dep_nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the name of the department');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'title',
-                message: 'Enter the title of the role:',
-                when: ({ choice }) => choice === 'Add a role'
+                message: 'Enter the title of the role: (Required)',
+                when: ({ choice }) => choice === 'Add a role',
+                validate: titleInput => {
+                    if (titleInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the job title of the role');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -51,22 +73,38 @@ const initialPrompt = () => {
                 when: ({ choice }) => choice === 'Add a role'
             },
             {
-                type: 'input',
+                type: 'list',
                 name: 'department_id',
                 message: 'Enter the deparment id for the role:',
-                when: ({ choice }) => choice === 'Add a role'
+                when: ({ choice }) => choice === 'Add a role',
             },
             {
                 type: 'input',
                 name: 'first_name',
-                message: "Enter the employee's First name:",
-                when: ({ choice }) => choice === 'Add an employee'
+                message: "Enter the employee's First name: (Required)",
+                when: ({ choice }) => choice === 'Add an employee',
+                validate: first_nameInput => {
+                    if (first_nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the First name of the Employee');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'last_name',
                 message: "Enter the employee's Last name:",
-                when: ({ choice }) => choice === 'Add an employee'
+                when: ({ choice }) => choice === 'Add an employee',
+                validate: last_nameInput => {
+                    if (last_nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the Last name of the Employee');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -93,60 +131,116 @@ const initialPrompt = () => {
                 when: ({ choice }) => choice === 'Update an employee role'
             },
         ])
+        .then(response => {
+            if (response.choice != "Quit") {
+                displayResult(response);
+                return true;
+            } else {
+                console.log(`You are exited out of the loop. Press crtl+C to exit out of the prompt`);
+                return false;
+            }
+        });
 };
 
-initialPrompt()
-.then(response => {
 
-    if (response.choice === "View all departments") {
-        db.query(queries.getAllDeps, (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-        console.table(data);
-        });
-    } if (response.choice === "View all roles") {
-        db.query(queries.getAllRoles, (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-            console.table(data);
-        });
-    } if (response.choice === "View all employees") {
-        db.query(queries.getAllEmps, (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-            console.table(data);
-        });
-    } if (response.choice === "Add a department") {
-        db.query(queries.addDep, [response.dep_name], (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(`${response.dep_name} added to the department table`);
-        });
-    } if (response.choice === "Add a role") {
-        db.query(queries.addRole, [response.title, response.salary, response.department_id], (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(`${response.title} added to the role table`);
-        });
-    } if (response.choice === "Add an employee") {
-        db.query(queries.addEmp, [response.first_name, response.last_name, response.role_id, response.manager_id], (err, data) => {
-            if (err) {
-               return console.log(err);
-            }
-            console.log(`Employee ${response.first_name} ${response.last_name} added to the employee table`);
-        });
-    } if (response.choice === "Update an employee role") {
-        db.query(queries.updateEmpRole, [response.role_id, response.emp_id], (err, data) => {
-            if (err) {
-                return console.log(err);
-            }
-            console.log(`Emp Id ${response.emp_id} updated in the employee table`);
-        });
+const displayResult = response => {
+    switch (response.choice) {
+
+        case "View all departments":
+            getDeps();
+            break;
+
+        case "View all roles":
+            getRoles();
+            break;
+
+        case "View all employees":
+            getEmps();
+            break;
+
+        case "Add a department":
+            addNewDep(response);
+            break;
+
+        case "Add a role":
+            addNewRole(response);
+            break;
+
+        case "Add an employee":
+            addNewEmp(response);
+            break;
+
+        case "Update an employee role":
+            updEmpRole(response);
+            break;
     }
-});
+};
 
+// function to get all departments
+const getDeps = () => {
+    dbpool.query(queries.getAllDeps, (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        initialPrompt();
+    });
+};
+
+// Function to get all roles
+const getRoles = () => {
+    dbpool.query(queries.getAllRoles, (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        initialPrompt();
+    });
+};
+
+// Function to get all Employees
+const getEmps = () => {
+    dbpool.query(queries.getAllEmps, (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        initialPrompt();
+    });
+};
+
+// Function to add a Department
+const addNewDep = response => {
+    dbpool.query(queries.addDep, [response.dep_name], (err, data) => {
+        if (err) throw err;
+        console.log(`${response.dep_name} added to the department table`);
+        getDeps();
+        initialPrompt();
+    });
+};
+
+// Function to add a Role
+const addNewRole = response => {
+    dbpool.query(queries.addRole, [response.title, response.salary, response.department_id], (err, data) => {
+        if (err) throw err;
+        console.log(`${response.title} added to the role table`);
+        getRoles();
+        initialPrompt();
+    });
+};
+
+// Function to add employee
+const addNewEmp = response => {
+    dbpool.query(queries.addEmp, [response.first_name, response.last_name, response.role_id, response.manager_id], (err, data) => {
+        if (err) throw err;
+        console.log(`Employee ${response.first_name} ${response.last_name} added to the employee table`);
+        getEmps();
+        initialPrompt();
+    });
+};
+
+// Function to updated an employee's role
+const updEmpRole = response => {
+    dbpool.query(queries.updateEmpRole, [response.role_id, response.emp_id], (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Emp Id ${response.emp_id} updated in the employee table`);
+        getEmps();
+        initialPrompt();
+    });
+};
