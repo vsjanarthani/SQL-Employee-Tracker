4b0db9f63dcbf14d495fa5ebda2a4803dd7e7742
jana <vs.janarthani@gmail.com>
2021-04-21 23:43:55 -0700

    code modularised

--- server.js
+++ server.js
@@ -1,91 +1,12 @@
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
-const mysql = require('mysql2');
-const queries = require('./lib/queries');
-const { Department, Role, Employee, EmployeebyMgr } = require('./lib/constructor');
-require('dotenv').config();
-
-
-
-// Connect to database
-const pool = mysql.createPool(
-    {
-        host: 'localhost',
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: 'employee_db',
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
-    }
-);
-
-const dbpool = pool.promise();
-
-// Function to get manager names
-async function getManagers() {
-    try {
-        let rowdata = await dbpool.query(queries.getAllManagers);
-        let managerlist = [];
-        rowdata = rowdata[0];
-        for (row in rowdata) {
-            managerlist.push(`${rowdata[row].manager_id} ${rowdata[row].manager_name}`);
-        }
-        return managerlist;
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-// Function to get department list
-async function getDeplist() {
-    try {
-        const depquery = await dbpool.query(queries.getAllDeps);
-        let depname = [];
-        let rowdata = depquery[0];
-        for (row in rowdata) {
-            depname.push(`${rowdata[row].dep_id} ${rowdata[row].dep_name}`);
-        }
-        return depname;
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-// Function to get role list
-async function getRoleList() {
-    try {
-        const rolequery = await dbpool.query(queries.getAllRoles);
-        let roleTitle = [];
-        let rowdata = rolequery[0];
-        for (row in rowdata) {
-            roleTitle.push(`${rowdata[row].role_id} ${rowdata[row].job_title}`);
-        }
-        return roleTitle;
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-// Function to get Employee list
-async function getEmpList() {
-    try {
-        const empquery = await dbpool.query(queries.getAllEmps);
-        let empNames = [];
-        let rowdata = empquery[0];
-        for (row in rowdata) {
-            empNames.push(`${rowdata[row].emp_id} ${rowdata[row].first_name} ${rowdata[row].last_name}`);
-        }
-        return empNames;
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-initialPrompt();
+const { getDeplist, getDeps, addNewDep, queryDept, deleteDep, budgetbyDep } = require('./lib/Functions/department');
+const { getRoleList, getRoles, addNewRole, queryRoleTitle, deleteRole } = require('./lib/Functions/role');
+const { getManagers, getEmpList, getEmps, addNewEmp, queryEmpName, updEmpRole, queryEmpID, updEmpMgr,
+    getEmpsbyMgr, displayData, getEmpsbyDept, deleteEmp } = require('./lib/Functions/employee');
 
 // Initial prompt
-async function initialPrompt() {
+const initialPrompt = async() => {
     let managers = await getManagers();
     let departments = await getDeplist();
     let roletitle = await getRoleList();
@@ -254,367 +175,78 @@ async function initialPrompt() {
         });
 };
 
+initialPrompt();
 
 const displayResult = response => {
     switch (response.choice) {
 
         case "View all departments":
-            getDeps();
-            initialPrompt();
+            getDeps().then(() => initialPrompt());
             break;
 
         case "View all roles":
-            getRoles();
-            initialPrompt();
+            getRoles().then(() => initialPrompt());
             break;
 
         case "View all employees":
-            getEmps();
-            initialPrompt();
+            getEmps().then(() => initialPrompt());
             break;
 
         case "Add a department":
-            addNewDep(response).then(response => queryDept(response));
+            addNewDep(response).then(response => queryDept(response))
+            .then(() => initialPrompt());
             break;
 
         case "Add a role":
-            addNewRole(response).then(response => queryRoleTitle(response));
+            addNewRole(response).then(response => queryRoleTitle(response))
+            .then(() => initialPrompt());
             break;
 
         case "Add an employee":
-            addNewEmp(response).then(response => queryEmpName(response));
+            addNewEmp(response).then(response => queryEmpName(response))
+            .then(() => initialPrompt());
             break;
 
         case "Update an employee's role":
-            updEmpRole(response).then(response => queryEmpID(response));
+            updEmpRole(response).then(response => queryEmpID(response))
+            .then(() => initialPrompt());
             break;
 
         case "Update an employee's Manager":
-            updEmpMgr(response).then(response => queryEmpID(response));
+            updEmpMgr(response).then(response => queryEmpID(response))
+            .then(() => initialPrompt());
             break;
 
         case "View employees by manager":
-            getEmpsbyMgr(response).then(response => displayData(response));
+            getEmpsbyMgr(response).then(response => displayData(response))
+            .then(() => initialPrompt());
             break;
 
         case "View employees by department":
-            getEmpsbyDept(response);
-            initialPrompt();
+            getEmpsbyDept(response)
+            .then(() => initialPrompt());
             break;
 
         case "Delete employee":
-            deleteEmp(response);
+            deleteEmp(response)
+            .then(() => initialPrompt());
             break;
 
         case "Delete role":
-            deleteRole(response);
+            deleteRole(response)
+            .then(() => initialPrompt());
             break;
 
         case "Delete department":
-            deleteDep(response);
+            deleteDep(response)
+            .then(() => initialPrompt());
             break;
         
         case "Get utilized budget by department":
-            budgetbyDep(response);
+            budgetbyDep(response)
+            .then(() => initialPrompt());
             break;
     }
 };
 
-// function to get all departments
-const getDeps = async () => {
-    try {
-        const depquery = await dbpool.query(queries.getAllDeps);
-        let departmentList = [];
-        let depname = [];
-        let rowdata = depquery[0];
-        for (row in rowdata) {
-            departmentList.push(new Department(rowdata[row].dep_id, rowdata[row].dep_name));
-            depname.push(`${rowdata[row].dep_id} ${rowdata[row].dep_name}`);
-        }
-        console.table(departmentList);
-        return depname;
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to get all roles
-const getRoles = async () => {
-    try {
-        const rolequery = await dbpool.query(queries.getAllRoles);
-        let roleList = [];
-        let rowdata = rolequery[0];
-        for (row in rowdata) {
-            roleList.push(new Role(rowdata[row].role_id, rowdata[row].job_title, rowdata[row].department, rowdata[row].salary));
-        }
-        console.table(roleList);
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to get all Employees
-const getEmps = async () => {
-    try {
-        const empquery = await dbpool.query(queries.getAllEmps);
-        let empList = [];
-        let rowdata = empquery[0];
-        for (row in rowdata) {
-            empList.push(new Employee(rowdata[row].emp_id, rowdata[row].first_name, rowdata[row].last_name, rowdata[row].job_title, rowdata[row].salary, rowdata[row].manager_name, rowdata[row].department));
-        }
-        if (empList.length <= 0) {
-            console.log(`No records to display`);
-        } else {
-            console.log('\n');
-            console.table(empList);
-        }  
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to add a Department
-const addNewDep = async response => {
-    try {
-        await dbpool.query(queries.addDep, response.dep_name);
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    return response.dep_name;
-};
-
-// Function to query a department by name
-const queryDept = async response => {
-    try {
-        const querybydep = await dbpool.query(queries.getbydep_name, response);
-        if (querybydep[0].length <= 0) {
-            console.log(`No records to display`);
-        } else {
-            console.log(`Department added successfully`);
-            console.table(querybydep[0]);
-        }
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// Function to add a Role
-const addNewRole = async response => {
-    let dep_id = parseInt(response.dep_name.split(' ')[0]);
-    try {
-        await dbpool.query(queries.addRole, [response.title, response.salary, dep_id]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    return response.title;
-};
-
-// Function to query a role by title
-const queryRoleTitle = async response => {
-    try {
-        const queryRole = await dbpool.query(queries.getbyrole_title, response);
-        if (queryRole[0].length <= 0) {
-            console.log(`No records to display`);
-        } else {
-            console.log(`\n Role added successfully`);
-            console.table(queryRole[0]);
-        }
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// Function to add an employee
-const addNewEmp = async response => {
-    let manager_id;
-    if (response.manager_name === 'None') {
-        manager_id = null;
-    } else {
-        manager_id = parseInt(response.manager_name.split(' ')[0]);
-    }
-    let role_id = parseInt(response.role_title.split(' ')[0]);
-    try {
-        await dbpool.query(queries.addEmp, [response.first_name, response.last_name, role_id, manager_id]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    return (`${response.first_name}_${response.last_name}`);
-};
-
-// Function to query employee by first and last name
-const queryEmpName = async response => {
-    try {
-        let first_name = (response.split('_')[0]);
-        let last_name = (response.split('_')[1]);
-        const queryNewEmp = await dbpool.query(queries.getbyemp_names, [first_name, last_name]);
-        if (queryNewEmp[0].length <= 0) {
-            console.log(`No records to display`);
-        } else {
-            console.log(`\n Employee added successfully`);
-            console.table(queryNewEmp[0]);
-        }
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// Function to updated an employee's role
-const updEmpRole = async response => {
-    try {
-        const emp_id = (response.emp_name.split(' ')[0]);
-        const role_id = (response.role_title.split(' ')[0]);
-        await dbpool.query(queries.updateEmpRole, [role_id, emp_id]);
-        return emp_id;
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to query Employee by Id
-const queryEmpID = async response => {
-    try {
-        const queryEmp = await dbpool.query(queries.getempbyId, [response]);
-        if (queryEmp[0].length <= 0) {
-            console.log(`No records to display`);
-        } else {
-            console.log(`\n Employee updated successfully`);
-            console.table(queryEmp[0]);
-        }
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// Function to update Employee's manager
-const updEmpMgr = async response => {
-    try {
-        let manager_id;
-        if (response.manager_name === 'None') {
-            manager_id = null;
-        } else {
-            manager_id = parseInt(response.manager_name.split(' ')[0]);
-        }
-        const emp_id = (response.emp_name.split(' ')[0]);
-        await dbpool.query(queries.updateEmpManager, [manager_id, emp_id]);
-        return emp_id;
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to view Employees by Manager
-const getEmpsbyMgr = async response => {
-    let manager_id;
-    try {
-        if (response.manager_name != 'None') {
-            manager_id = parseInt(response.manager_name.split(' ')[0]);
-            const empquery = await dbpool.query(queries.getEmpsbyManager, manager_id);
-            return empquery[0];
-        } else {
-            const empquery = await dbpool.query(queries.getEmpsbyNoManager);
-            return empquery[0];
-        }
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to display the employee data by manager
-const displayData = response => {
-    let empList = [];
-    for (row in response) {
-        empList.push(new EmployeebyMgr(response[row].emp_id, response[row].first_name, response[row].last_name, response[row].job_title, response[row].salary, response[row].manager_name));
-    }
-    if (empList.length <= 0) {
-        console.log(`No Employees to display`);
-    } else {
-        console.log('\n');
-        console.table(empList);
-    }
-    initialPrompt();
-};
-
-// Function to get employees by department
-const getEmpsbyDept = async response => {
-    try {
-        let dep_id = parseInt(response.dep_name.split(' ')[0]);
-        const queryempbydep = await dbpool.query(queries.getEmpsbyDep, dep_id);
-        let empList = [];
-        let rowdata = queryempbydep[0];
-        for (row in rowdata) {
-            empList.push(new Employee(rowdata[row].emp_id, rowdata[row].first_name, rowdata[row].last_name, rowdata[row].job_title, rowdata[row].salary, rowdata[row].manager_name, rowdata[row].department));
-        }
-        if (empList.length <= 0) {
-            console.log(`No Employees to display`);
-            return;
-        }
-        console.log('\n');
-        console.table(empList);
-    } catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-};
-
-// Function to delete employee by id
-const deleteEmp = async response => {
-    try {
-        const emp_id = (response.emp_name.split(' ')[0]);
-        const delEmp = await dbpool.query(queries.deleteEmpbyId, emp_id);
-        console.log(delEmp[0]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// function to delete role by id
-const deleteRole = async response => {
-    try {
-        const role_id = (response.role_title.split(' ')[0]);
-        const delrole = await dbpool.query(queries.deleteRolebyId, role_id);
-        console.log(delrole[0]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// function to delete deparment by id
-const deleteDep = async response => {
-    try {
-        let dep_id = parseInt(response.dep_name.split(' ')[0]);
-        const deldep = await dbpool.query(queries.deletedepbyId, dep_id);
-        console.log(deldep[0]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
-
-// function to get budget by deparment
-const budgetbyDep = async response => {
-    try {
-        let dep_id = parseInt(response.dep_name.split(' ')[0]);
-        const getbudget = await dbpool.query(queries.getSalarybyDep_id, dep_id);
-        console.table(getbudget[0]);
-    }
-    catch (err) {
-        console.error(`Error in executing Query ${err}`);
-    }
-    initialPrompt();
-};
+module.exports = { initialPrompt };
